252e0df12f63e132babf8c6cdbe2944281b66840
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-05-12 21:13:22 +0200

    Implement choice mechanism

--- English/crossword.py
+++ English/crossword.py
@@ -5,12 +5,23 @@ for the crossword puzzle https://crosswordlabs.com/ website.
 """
 from dataclasses import dataclass
 from pathlib import Path
-from random import shuffle
+from random import shuffle, randint
+from typing import List
+from enum import IntEnum
+from string import ascii_lowercase, ascii_uppercase
 
-IMPORT_VOCABULARIES_PATH = Path(__file__).parent.parent / "files" / "english" / "raw_vocabularies"
-PAYLOAD_TO_CROSSWORD_PATH = Path(__file__).parent.parent / "files" / "english" / "payload_to_crossword"
+DIR_BASE_SAVE = Path(__file__).parent.parent / "files" / "english"
+IMPORT_VOCABULARIES_PATH = DIR_BASE_SAVE / "raw_vocabularies"
+PAYLOAD_TO_CROSSWORD_PATH = DIR_BASE_SAVE/ "payload_to_crossword"
+CHOICES_PATH = DIR_BASE_SAVE / "choices"
 
 LIST_OF_VOCABULARIES = []
+
+class SuffixEnum(IntEnum):
+    ALPHABET_LOWER = 1
+    NUMBER = 2
+    ALPHABET_UPPER = 3
+
 @dataclass
 class Vocabulary:
     english: str
@@ -69,6 +80,46 @@ def save_voc_to_file(path: Path = PAYLOAD_TO_CROSSWORD_PATH):
             # Write the prepared payload for each vocabulary to the file
             file.write(voc.prepare_payload_to_crossword() + "\n")
 
+def choice_words(list_of_voc: List[Vocabulary], labels: List[str]):
+    """
+    Shuffles the list of vocabulary objects and creates a new list of lists of mixed words.
+    """
+    mixed = []
+    for label in labels:
+        helper_list = [getattr(attr, label) for attr in list_of_voc]
+        shuffle(helper_list)
+        mixed.append(helper_list)
+    return mixed
+
+def save_choice_words(list_of_choices: List[List[str]], path_name: Path| str = CHOICES_PATH):
+    with open(path_name, "w", encoding="utf-8") as file:
+        for idx_suffix, words in enumerate(list_of_choices):
+            for idx, word in enumerate(words):
+                payload = set_suffix(SuffixEnum(idx_suffix+1), idx) + ". " + word
+                file.write(payload + "\n")
+            file.write("\n" + '*'*50 + "\n")
+
+def set_suffix(suffix: SuffixEnum, position: int):
+    match suffix:
+        case SuffixEnum.ALPHABET_LOWER:
+            return ascii_lowercase[position]
+        case SuffixEnum.NUMBER:
+            return str(position)
+        case SuffixEnum.ALPHABET_UPPER:
+            return ascii_uppercase[position]
+        case _:
+            raise NotImplementedError(f"Case not implemented {suffix}")
+
+def chose_choice_words(list_of_voc: List[Vocabulary])-> List[List[str]]:
+    rand = randint(1,2)
+    match rand:
+        case 1:
+            return choice_words(list_of_voc, ["english", "definition", "polish"])
+        case 2:
+            return choice_words(list_of_voc, ["english", "sentence_with_gap"])
+        case _:
+            raise NotImplementedError(f"Case not implemented {rand}")
+
 if __name__ == "__main__":
     print("Importing vocabularies")
     import_voc_from_file_to_list()
@@ -77,3 +128,8 @@ if __name__ == "__main__":
     print("Saving vocabularies")
     save_voc_to_file()
     print("Vocabularies have been saved")
+    print("Creating choices")
+    choices = chose_choice_words(LIST_OF_VOCABULARIES)
+    print("Saving choices")
+    save_choice_words(choices)
+    print("Choices have been saved")
