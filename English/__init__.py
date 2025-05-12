3c72caae7bbeff3f4277e5eb1e3a6dacdcb04660
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-05-12 21:50:48 +0200

    Document structure of English package in __init__

---
+++ English/__init__.py
@@ -0,0 +1,9 @@
+"""
+The English package provides tools to manage and transform vocabulary data
+for language learning applications.
+
+Modules:
+- vocabulary: Defines the Vocabulary data model and import/export utilities.
+- choice_generator: Generates randomized multiple choice data for revision exercises.
+- crossword_export: Prepares vocabulary data for crossword puzzle generation (e.g. via crosswordlabs.com).
+"""
