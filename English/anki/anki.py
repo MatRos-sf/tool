cb39a6c2bbfbb993a979fc4fccbef84d3fe1f54b
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-05-14 21:46:37 +0200

    feat: create function to create csv file for Anki

---
+++ English/anki/anki.py
@@ -0,0 +1,21 @@
+import csv
+from typing import List
+from pathlib import Path
+from datetime import datetime
+
+from English.vocabulary import Vocabulary
+from English.anki.field import Extend
+
+def create_anki_csv(path: Path, list_of_vocabularies: List[Vocabulary]):
+    with open(path, "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        for voc in list_of_vocabularies:
+            row = []
+            for field in Extend:
+                if hasattr(voc, field):
+                    row.append(getattr(voc, field))
+                if field == Extend.TAG:
+                    row.append(datetime.strftime(datetime.now(), "%b").lower())
+                else:
+                    row.append("")
+            writer.writerow(row)
---
+++ English/anki/field.py
@@ -0,0 +1,16 @@
+from enum import StrEnum
+
+class Extend(StrEnum):
+    ENGLISH = "english"
+    AUDIO_ENGLISH = "audio_english"
+    PHONETIC = "phonetic"
+    PART_OF_SPEACH = "part_of_speach"
+    DEFINITION = "definition"
+    POLISH = "polish"
+    SENTENCE = "sentence"
+    AUDIO_SENTENCE = "audio_sentence"
+    SENTENCE_WITH_GAP = "sentence_with_gap"
+    DEF_TIP = "def-tip"
+    TAG = "tag"
+
+
