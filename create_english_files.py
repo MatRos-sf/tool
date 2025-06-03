e4e16621d713e34523bee47feb61405bad0ebbe1
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-06-03 22:08:27 +0200

    Add new arg mix

--- create_english_files.py
+++ create_english_files.py
@@ -1,16 +1,15 @@
-from pathlib import Path
-from random import shuffle
 import argparse
-from typing import List
 import os
 import warnings
+from pathlib import Path
+from random import shuffle
+from typing import List
 
-from English.vocabulary import import_voc_from_file_to_list
-from English.crossword import save_voc_to_file
-from English.choice_section import choose_choice_words, save_choice_words
-from English.anki import create_anki_csv, ANKI_FIELDS
+from English.anki import ANKI_FIELDS, create_anki_csv
 from English.browser import open_browser_with_vocabularies
-
+from English.choice_section import choose_choice_words, save_choice_words
+from English.crossword import save_voc_to_file
+from English.vocabulary import import_voc_from_file_to_list
 
 
 def command_separated_list(value) -> List[int]:
@@ -19,7 +18,10 @@ def command_separated_list(value) -> List[int]:
     except ValueError:
         raise argparse.ArgumentTypeError("List of integers expected")
 
-parser = argparse.ArgumentParser(description="Script to create English files for: crossword, choices and anki")
+
+parser = argparse.ArgumentParser(
+    description="Script to create English files for: crossword, choices and anki"
+)
 
 parser.add_argument(
     "--path_raw",
@@ -32,35 +34,50 @@ parser.add_argument(
 parser.add_argument(
     "--tag",
     type=command_separated_list,
-    help='Comma-separated list of integers where 1 = crossword, 2 = choices, 3 = anki',
-    default=[1, 2, 3]
+    help="Comma-separated list of integers where 1 = crossword, 2 = choices, 3 = anki",
+    default=[1, 2, 3],
 )
 parser.add_argument(
-    '--type_anki_field',
-    help = "Type of anki field. Anki field are implemented in anki/filed directory with ANKI_FIELD variables."
-           "If you want create new one please implemented it in ANKI_FIELD dictionary. ",
-    default = "extend"
+    "--type_anki_field",
+    help="Type of anki field. Anki field are implemented in anki/filed directory with ANKI_FIELD variables."
+    "If you want create new one please implemented it in ANKI_FIELD dictionary. ",
+    default="extend",
+)
+
+parser.add_argument(
+    "--mix",
+    type=bool,
+    default=False,
+    help="If true, vocabularies will be shuffled before processing",
 )
 
 args = parser.parse_args()
 print("Importing vocabularies")
-list_of_vocabularies = import_voc_from_file_to_list(Path(args.path_raw) / Path("raw_vocabularies"))
-print("Shuffling vocabularies")
-shuffle(list_of_vocabularies)
+list_of_vocabularies = import_voc_from_file_to_list(
+    Path(args.path_raw) / Path("raw_vocabularies")
+)
+
+if args.mix:
+    print("Shuffling vocabularies")
+    shuffle(list_of_vocabularies)
 
 # GUARD
 if args.type_anki_field != "extend":
     if 3 not in args.tag:
-        warnings.warn("You can't use --type_anki_field without --tag 3, so --type_anki_field will be ignored")
+        warnings.warn(
+            "You can't use --type_anki_field without --tag 3, so --type_anki_field will be ignored"
+        )
     elif not ANKI_FIELDS.get(args.type_anki_field.title()):
-        raise ValueError(f"Type of anki field {args.type_anki_field} is not implemented. You can only use: {', '.join(ANKI_FIELDS.keys())}")
+        raise ValueError(
+            f"Type of anki field {args.type_anki_field} is not implemented. You can only use: {', '.join(ANKI_FIELDS.keys())}"
+        )
 
 field = ANKI_FIELDS[args.type_anki_field.lower()]
 
 output_path = {
     1: Path(args.path_raw) / "crossword.txt",
     2: Path(args.path_raw) / "choices.txt",
-    3: Path(args.path_raw) / "anki.csv"
+    3: Path(args.path_raw) / "anki.csv",
 }
 
 for tag in args.tag:
